aa6066636dc5e0cfdfd9b23a8cc31fb175b7dd5b
Lan Wang <dlwlan@cn.ibm.com>
2016-01-25 15:38:07 +0800

    Caller(R108,R110,R150)

---
+++ OIMSimpCHWPrototype/src/com/ibm/rdh/chw/caller/R110assignMODCharacteristicToModelsClass.java
@@ -0,0 +1,125 @@
+package com.ibm.rdh.chw.caller;
+
+import com.ibm.pprds.epimshw.HWPIMSAbnormalException;
+import com.ibm.rdh.chw.entity.CHWAnnouncement;
+import com.ibm.rdh.chw.entity.TypeModel;
+import com.ibm.rdh.rfc.RmclmTable;
+import com.ibm.rdh.rfc.RmclmTableRow;
+import com.ibm.rdh.rfc.Z_DM_SAP_ASSIGN_CHAR_TO_CLASS;
+import com.ibm.rdh.rfc.Zdm_geo_to_classTable;
+import com.ibm.rdh.rfc.Zdm_geo_to_classTableRow;
+
+public class R110assignMODCharacteristicToModelsClass extends Rfc {
+
+	private com.ibm.rdh.rfc.Z_DM_SAP_ASSIGN_CHAR_TO_CLASS rfc;
+
+	public R110assignMODCharacteristicToModelsClass(TypeModel typeModel,
+			CHWAnnouncement chwA, String pimsIdentity) throws Exception {
+
+		reInitialize();
+		rfcName = "Z_DM_SAP_ASSIGN_CHAR_TO_CLASS";
+
+		rfc = new com.ibm.rdh.rfc.Z_DM_SAP_ASSIGN_CHAR_TO_CLASS();
+
+		// Set up the RFC fields
+		// C0
+		rfc.setJClass("MK_" + typeModel.getType() + "_MODELS");
+		rfc.setJKlart("300");
+
+		rfcInfo.append("Direct fields \n");
+		rfcInfo.append(Tab + "CLASS>>" + rfc.getJClass() + ", KLART>>"
+				+ rfc.getJKlart() + "\n");
+
+		// RMCLM - V1
+		RmclmTable c1Table = new RmclmTable();
+		RmclmTableRow c1Row = c1Table.createEmptyRow();
+
+		c1Row.setMerkma("MK_" + typeModel.getType() + "_MOD");
+
+		c1Table.appendRow(c1Row);
+
+		rfc.setJMultichar(c1Table);
+
+		rfcInfo.append("RMCLM \n");
+		rfcInfo.append(Tab + "MERKMA>>" + c1Row.getMerkma() + "\n");
+
+		Zdm_geo_to_classTable zdmTable = new Zdm_geo_to_classTable();
+		Zdm_geo_to_classTableRow zdmRow = zdmTable.createEmptyRow();
+
+		zdmRow.setZGeo("US");
+
+		zdmTable.appendRow(zdmRow);
+
+		rfc.setGeoData(zdmTable);
+
+		rfcInfo.append("ZDM_GEO_TO_CLASS \n");
+		rfcInfo.append(Tab + "GEO>>" + zdmRow.getZGeo() + "\n");
+
+		rfc.setPimsIdentity(pimsIdentity);
+		rfcInfo.append("PIMSIdentity \n");
+		rfcInfo.append(Tab + "PIMSIdentity>>" + pimsIdentity + "\n");
+
+		// RFANUMBER
+		rfc.setRfaNum(chwA.getAnnDocNo());
+		rfcInfo.append("RFANUM \n");
+		rfcInfo.append(Tab + "RFANumber>>" + chwA.getAnnDocNo() + "\n");
+
+	}
+
+	@Override
+	public void execute() throws Exception {
+		logExecution();
+		getRfc().execute();
+		getLog().debug(getErrorInformation());
+		if (getSeverity() == ERROR) {
+			throw new HWPIMSAbnormalException(getErrorInformation());
+		}
+	}
+
+	@Override
+	public String getTaskDescription() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(" " + getMaterialName());
+		return sb.toString();
+	}
+
+	@Override
+	protected boolean isSuccessful() {
+		boolean ans = false;
+		int rc = getRfc().getRfcrc();
+		if (0 == rc) {
+			ans = true;
+		}
+		return ans;
+	}
+
+	public Z_DM_SAP_ASSIGN_CHAR_TO_CLASS getRfc() {
+		return rfc;
+	}
+	
+	@Override
+	protected String getErrorInformation() {
+		String ans;
+		if (isSuccessful()) {
+			ans = RFC_OK_MESSAGE;
+		} else {
+			ans = formatRfcErrorMessage(getRfc().getRfcrc(), getRfc()
+					.getErrorText());
+		}
+		return ans;
+	}
+
+	@Override
+	protected String getMaterialName() {
+		return "Assign MOD characteristic to MODELS class";
+	}
+
+	@Override
+	public String getRfcName() {
+		return ClassUtil.getSimpleClassName(getRfc());
+	}
+
+	public void evaluate() throws Exception {
+		execute();
+	}
+}
---
+++ OIMSimpCHWPrototype/src/com/ibm/rdh/chw/caller/R150create012ClassificationForMOD.java
@@ -0,0 +1,219 @@
+package com.ibm.rdh.chw.caller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import com.ibm.pprds.epimshw.HWPIMSAbnormalException;
+import com.ibm.pprds.epimshw.PropertyKeys;
+import com.ibm.pprds.epimshw.util.ConfigManager;
+import com.ibm.rdh.chw.entity.CHWAnnouncement;
+import com.ibm.rdh.chw.entity.TypeModel;
+import com.ibm.rdh.rfc.Api_auspTable;
+import com.ibm.rdh.rfc.Api_auspTableRow;
+import com.ibm.rdh.rfc.KlahTable;
+import com.ibm.rdh.rfc.KlahTableRow;
+import com.ibm.rdh.rfc.KsskTable;
+import com.ibm.rdh.rfc.KsskTableRow;
+import com.ibm.rdh.rfc.MaraTable;
+import com.ibm.rdh.rfc.MaraTableRow;
+import com.ibm.rdh.rfc.Object_keyTable;
+import com.ibm.rdh.rfc.Object_keyTableRow;
+import com.ibm.rdh.rfc.RcucoTable;
+import com.ibm.rdh.rfc.RcucoTableRow;
+import com.ibm.rdh.rfc.Z_DM_SAP_CLASSIFICATION_MAINT;
+import com.ibm.rdh.rfc.Zdm_geo_to_classTable;
+import com.ibm.rdh.rfc.Zdm_geo_to_classTableRow;
+
+public class R150create012ClassificationForMOD extends Rfc {
+
+	private String sDateFormat = null;
+	private com.ibm.rdh.rfc.Z_DM_SAP_CLASSIFICATION_MAINT rfc;
+
+	public R150create012ClassificationForMOD(TypeModel typeModel,
+			CHWAnnouncement chwA, String pimsIdentity) throws Exception {
+		reInitialize();
+		Date curDate = new Date();
+		sDateFormat = ConfigManager.getConfigManager().getString(
+				PropertyKeys.KEY_DATE_FORMAT, true);
+		SimpleDateFormat sdf = new SimpleDateFormat(sDateFormat);
+		rfcName = "Z_DM_SAP_CLASSIFICATION_MAINT";
+		Api_auspTableRow r6Row;
+		int rowCnt;
+
+		rfc = new com.ibm.rdh.rfc.Z_DM_SAP_CLASSIFICATION_MAINT();
+
+		// Set up the RFC fields
+		// OBJECT_KEY - R0
+		Object_keyTable r0Table = new Object_keyTable();
+		Object_keyTableRow r0Row = r0Table.createEmptyRow();
+
+		r0Row.setKeyFeld("ATNAM");
+		r0Row.setKparaValu("MK_" + typeModel.getType() + "_MOD");
+
+		r0Table.appendRow(r0Row);
+		rfc.setIObjectKey(r0Table);
+
+		rfcInfo.append("OBJECTKEY \n");
+		rfcInfo.append(Tab + "KEYFELD>>" + r0Row.getKeyFeld() + ", KPARAVALU>>"
+				+ r0Row.getKparaValu() + "\n");
+
+		// KLAH - R2
+		KlahTable r2Table = new KlahTable();
+		KlahTableRow r2Row = r2Table.createEmptyRow();
+
+		r2Row.setClass("MK_PRODUCT_CHARS");
+
+		r2Table.appendRow(r2Row);
+		rfc.setIKlah(r2Table);
+
+		rfcInfo.append("KLAH \n");
+		rfcInfo.append(Tab + "CLASS>>" + r2Row.get_Class() + "\n");
+
+		// KSSK - R3
+		KsskTable r3Table = new KsskTable();
+		KsskTableRow r3Row = r3Table.createEmptyRow();
+
+		r3Row.setKlart("012");
+
+		r3Table.appendRow(r3Row);
+		rfc.setIKssk(r3Table);
+
+		rfcInfo.append("KSSK \n");
+		rfcInfo.append(Tab + "KLART>>" + r3Row.getKlart() + "\n");
+
+		// RCUCO - R4
+		RcucoTable r4Table = new RcucoTable();
+		RcucoTableRow r4Row = r4Table.createEmptyRow();
+
+		r4Row.setObtab("CABN");
+
+		r4Table.appendRow(r4Row);
+		rfc.setIRcuco(r4Table);
+
+		rfcInfo.append("RCUCO \n");
+		rfcInfo.append(Tab + "OBTAB>>" + r4Row.getObtab() + "\n");
+
+		// MARA - R5
+		MaraTable r5Table = new MaraTable();
+		MaraTableRow r5Row = r5Table.createEmptyRow();
+
+		// Passing date
+		// r5Row.setErsdaString(sdf.format(curDate));
+		r5Row.setErsda(curDate);
+
+		r5Table.appendRow(r5Row);
+		rfc.setIMara(r5Table);
+
+		rfcInfo.append("MARA  \n");
+		rfcInfo.append(Tab + "ERSDA>>" + r5Row.getErsdaString() + "\n");
+
+		// API_AUSP - R6
+		Api_auspTable r6Table = new Api_auspTable();
+		Vector r6Vector = new Vector(1, 1);
+
+		r6Row = r6Table.createEmptyRow();
+		r6Vector.addElement(r6Row);
+		r6Row.setCharact("MK_CHARACTERISTIC_TYPE");
+
+		r6Row.setValue("MODEL");
+		r6Row = r6Table.createEmptyRow();
+		r6Vector.addElement(r6Row);
+		r6Row.setCharact("MK_SALES_RELEVANCY");
+		r6Row.setValue("PRICING");
+
+		Enumeration em6 = r6Vector.elements();
+
+		rowCnt = 0;
+		while (em6.hasMoreElements()) {
+			r6Row = (Api_auspTableRow) em6.nextElement();
+			rowCnt++;
+			rfcInfo.append("API_AUSP Row " + rowCnt + "\n");
+			rfcInfo.append(Tab + "CHARACT>>" + r6Row.getCharact()
+					+ ", VALUE>> " + r6Row.getValue() + "\n");
+
+			// Add the row to the table
+			r6Table.appendRow(r6Row);
+		}
+		rfc.setIApiAusp(r6Table);
+
+		Zdm_geo_to_classTable zdmTable = new Zdm_geo_to_classTable();
+		Zdm_geo_to_classTableRow zdmRow = zdmTable.createEmptyRow();
+
+		zdmRow.setZGeo("US");
+
+		zdmTable.appendRow(zdmRow);
+
+		rfc.setGeoData(zdmTable);
+
+		rfcInfo.append("ZDM_GEO_TO_CLASS \n");
+		rfcInfo.append(Tab + "GEO>>" + zdmRow.getZGeo() + "\n");
+
+		rfc.setPimsIdentity(pimsIdentity);
+		rfcInfo.append("PIMSIdentity \n");
+		rfcInfo.append(Tab + "PIMSIdentity>>" + pimsIdentity + "\n");
+
+		// RFANUMBER
+		rfc.setRfaNum(chwA.getAnnDocNo());
+		rfcInfo.append("RFANUM \n");
+		rfcInfo.append(Tab + "RFANumber>>" + chwA.getAnnDocNo() + "\n");
+	}
+
+	@Override
+	public void execute() throws Exception {
+		logExecution();
+		getRfc().execute();
+		getLog().debug(getErrorInformation());
+		if (getSeverity() == ERROR) {
+			throw new HWPIMSAbnormalException(getErrorInformation());
+		}
+	}
+
+	@Override
+	public String getTaskDescription() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(" " + getMaterialName());
+		return sb.toString();
+	}
+
+	@Override
+	protected boolean isSuccessful() {
+		boolean ans = false;
+		int rc = getRfc().getRfcrc();
+		if (0 == rc) {
+			ans = true;
+		}
+		return ans;
+	}
+
+	public Z_DM_SAP_CLASSIFICATION_MAINT getRfc() {
+		return rfc;
+	}
+
+	@Override
+	protected String getErrorInformation() {
+		String ans;
+		if (isSuccessful()) {
+			ans = RFC_OK_MESSAGE;
+		} else {
+			ans = formatRfcErrorMessage(getRfc().getRfcrc(), getRfc()
+					.getErrorText());
+		}
+		return ans;
+	}
+
+	@Override
+	protected String getMaterialName() {
+		return "Create 012 Classification for MOD";
+	}
+
+	@Override
+	public String getRfcName() {
+		return ClassUtil.getSimpleClassName(getRfc());
+	}
+
+	public void evaluate() throws Exception {
+		execute();
+	}
+}
