b3dbd73450e8f08897d5fed0294a2fede936060a
Xue Mei Jiang <dljxuem@cn.ibm.com>
2016-03-30 19:45:21 +0800

    unit test changes

--- OIMSimpCHWPrototype/test/com/ibm/rdh/rfc/proxy/R172deleteModelValueFromTypeMODCharacteristicTest.java
+++ OIMSimpCHWPrototype/test/com/ibm/rdh/rfc/proxy/R172deleteModelValueFromTypeMODCharacteristicTest.java
@@ -14,12 +14,11 @@ import com.ibm.pprds.epimshw.util.LogManager;
 import com.ibm.rdh.chw.entity.CHWAnnouncement;
 import com.ibm.rdh.chw.entity.TypeModel;
 
-public class R133updateMaterialBasicViewForTypeModelTest extends
+public class R172deleteModelValueFromTypeMODCharacteristicTest extends
 		RdhRestProxyTest {
+
 	private static Logger logger = LogManager.getLogManager()
 			.getPromoteLogger();
-
-	String activeId = "Z_DM_SAP_MATM_CREATE";
 
 	@Before
 	public void prepareData() {
@@ -35,43 +34,35 @@ public class R133updateMaterialBasicViewForTypeModelTest extends
 	}
 
 	@Test
-	public void r133() {
+	public void r172() {
 		try {
-			CHWAnnouncement chwA = new CHWAnnouncement();
 			TypeModel typeModel = new TypeModel();
+			CHWAnnouncement chwA = new CHWAnnouncement();
 
-			chwA.setAnnDocNo("123401");
-			chwA.setAnnouncementType("New");
 			typeModel.setType("EACM");
-			typeModel.setModel("newCreate");
-			typeModel.setDiv("B1");
-			typeModel.setEanUPCCode("");
-			typeModel.setProductHierarchy("ph");
-			typeModel.setDescription("new create model");
+			typeModel.setModel("NEW");
+			chwA.setAnnDocNo("123401");
 			String pimsIdentity = "C";
-			String objectId = typeModel.getType() + typeModel.getModel();
+			String charac = "MK_" + typeModel.getType() + "_MOD";
+			String activeId = "Z_DM_SAP_CHAR_MAINTAIN";
 
-			int deleteDataResult = deleteDataMatmCreate(objectId);
-			assertEquals(deleteDataResult, 0);
-
-			deletezdmLogHdrAndzdmLogDtl(Constants.MANDT, activeId, objectId);
 			RdhRestProxy rfcProxy = new RdhRestProxy();
-			rfcProxy.r133(typeModel, chwA, pimsIdentity);
+			rfcProxy.r172(typeModel, chwA, pimsIdentity);
 
 			Map<String, String> map = new HashMap<String, String>();
 			Map<String, Object> rowDetails;
 
 			map.clear();
 			map.put("MANDT", "'" + Constants.MANDT + "'");
-			map.put("ZDMOBJKEY", "'" + objectId + "'");
-			map.put("ZDMOBJTYP", "'MAT'");
+			map.put("ZDMOBJKEY", "'" + charac + "'");
+			map.put("ZDMOBJTYP", "'CHR'");
 			rowDetails = selectTableRow(map, "ZDM_PARKTABLE");
 			assertNotNull(rowDetails);
 
 			map.clear();
 			map.put("MANDT", "'" + Constants.MANDT + "'");
-			map.put("ACTIV_ID", "'Z_DM_SAP_MATM_CREATE'");
-			map.put("OBJECT_ID", "'" + objectId + "'");
+			map.put("ACTIV_ID", "'" + activeId + "'");
+			map.put("OBJECT_ID", "'" + charac + "'");
 			rowDetails = selectTableRow(map, "ZDM_LOGHDR");
 			assertNotNull(rowDetails);
 			String sessionId = (String) rowDetails.get("ZSESSION");
@@ -80,33 +71,35 @@ public class R133updateMaterialBasicViewForTypeModelTest extends
 
 			map.clear();
 			map.put("ZSESSION", "'" + sessionId + "'");
-			map.put("TEXT",
-					"'Material Master created/updated successfully: EACMnewCreate'");
+			map.put("TEXT", "'Characteristic  " + charac
+					+ " created/updated successfully.'");
 			rowDetails = selectTableRow(map, "ZDM_LOGDTL");
 			assertNotNull(rowDetails);
 
 		} catch (HWPIMSAbnormalException ex) {
-
 			logger.info("error message= " + ex.getMessage());
 			Assert.fail("error message= " + ex.getMessage());
+
 		} catch (Exception e) {
 			e.getStackTrace();
 			Assert.fail("There is some error :" + e.getMessage());
 		} finally {
 
 		}
-
 	}
 
 	@After
 	public void deleteData() {
-		String del_rdx = "delete from sapr3.zdm_rdxcustmodel where mandt='200' and zdm_class='MD_CHW_NA' and zdm_syst_default='X'";
-		int t1 = SqlHelper.runUpdateSql(del_rdx, conn);
 
-		if (t1 >= 0) {
+		String del_cabn = "delete from SAPR3.CABN where mandt='200' and ATNAM='MK_EACM_MOD'";
+		String del_rdx = "delete from sapr3.zdm_rdxcustmodel where mandt='200' and zdm_class='MD_CHW_NA' and zdm_syst_default='X'";
+		int t1 = SqlHelper.runUpdateSql(del_cabn, conn);
+		int t2 = SqlHelper.runUpdateSql(del_rdx, conn);
+		if (t1 >= 0 && t2 >= 0) {
 			System.out.println("delete success");
 		} else {
 			System.out.println("delete failed");
 		}
 	}
+
 }
--- OIMSimpCHWPrototype/test/com/ibm/rdh/rfc/proxy/R262createPlantViewProfitCenterForMaterialTest.java
+++ OIMSimpCHWPrototype/test/com/ibm/rdh/rfc/proxy/R262createPlantViewProfitCenterForMaterialTest.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.ibm.pprds.epimshw.HWPIMSAbnormalException;
@@ -17,6 +19,19 @@ public class R262createPlantViewProfitCenterForMaterialTest extends
 			.getPromoteLogger();
 
 	String activeId = "Z_DM_SAP_MATM_CREATE";
+	
+	@Before
+	public void prepareData() {
+		String sql_rdx = "insert into sapr3.zdm_rdxcustmodel(mandt,zdm_class,zdm_syst_default) values ('200','MD_CHW_NA','X')";
+		
+		int t1 = SqlHelper.runUpdateSql(sql_rdx, conn);
+
+		if (t1 >= 0 ) {
+			System.out.println("insert success");
+		} else {
+			System.out.println("insert failed");
+		}
+	}
 
 	@Test
 	public void r262() {
@@ -75,5 +90,17 @@ public class R262createPlantViewProfitCenterForMaterialTest extends
 
 		}
 
+	}
+	@After
+	
+	public void deleteData() {
+		String del_rdx = "delete from sapr3.zdm_rdxcustmodel where mandt='200' and zdm_class='MD_CHW_NA' and zdm_syst_default='X'"; 
+		int t1 = SqlHelper.runUpdateSql(del_rdx, conn);
+
+		if (t1 >= 0 ) {
+			System.out.println("delete success");
+		} else {
+			System.out.println("delete failed");
+		}
 	}
 }
