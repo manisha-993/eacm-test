40c371976960a4e8be39226b2c3ec359fd63d896
Lan Wang <dlwlan@cn.ibm.com>
2016-04-18 15:52:58 +0800

    R144 junit

---
+++ OIMSimpCHWPrototype/test/com/ibm/rdh/rfc/proxy/R144updateParkStatusTest.java
@@ -0,0 +1,59 @@
+package com.ibm.rdh.rfc.proxy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.ibm.pprds.epimshw.HWPIMSAbnormalException;
+import com.ibm.pprds.epimshw.util.LogManager;
+
+public class R144updateParkStatusTest extends RdhRestProxyTest {
+
+	private static Logger logger = LogManager.getLogManager()
+			.getPromoteLogger();
+
+	@Test
+	public void testr144() {
+		try {
+			String annno = "123401";
+			String zdmstatus = "R";
+			String pimsIdentity = "C";
+			deletezdmLogHdrAndzdmLogDtl(Constants.MANDT,
+					"Z_DM_SAP_PARK_STATUS", annno);
+
+			RdhRestProxy rfcProxy = new RdhRestProxy();
+			rfcProxy.r144(annno, zdmstatus, pimsIdentity);
+
+			Map<String, String> map = new HashMap<String, String>();
+			Map<String, Object> rowDetails;
+
+			map.clear();
+			map.put("MANDT", "'" + Constants.MANDT + "'");
+			map.put("ACTIV_ID", "'Z_DM_SAP_PARK_STATUS'");
+			map.put("OBJECT_ID", annno);
+			rowDetails = selectTableRow(map, "ZDM_LOGHDR");
+			assertNotNull(rowDetails);
+			String sessionId = (String) rowDetails.get("ZSESSION");
+			String status = (String) rowDetails.get("STATUS");
+			assertEquals(status, "success");
+
+			map.clear();
+			map.put("ZSESSION", "'" + sessionId + "'");
+			map.put("TEXT", "'Success'");
+			rowDetails = selectTableRow(map, "ZDM_LOGDTL");
+			assertNotNull(rowDetails);
+
+		} catch (HWPIMSAbnormalException ex) {
+			logger.info("error message= " + ex.getMessage());
+			Assert.fail("error message= " + ex.getMessage());
+		} catch (Exception e) {
+			e.getStackTrace();
+			Assert.fail("There is some error :" + e.getMessage());
+		} finally {
+
+		}
+	}
+}
