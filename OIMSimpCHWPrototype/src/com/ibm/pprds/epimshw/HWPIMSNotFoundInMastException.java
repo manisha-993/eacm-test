aa8d32682697013342eed53d46cf19d137e0d7f7
Feng Liu <fliudl@cn.ibm.com>
2017-01-11 17:48:29 +0800

    Defect 1634558 - a problem in RFCABR on how to check on return back from BOM read RFC to determine if a BOM exists or not.

--- OIMSimpCHWPrototype/src/com/ibm/pprds/epimshw/HWPIMSNotFoundInMastException.java
+++ OIMSimpCHWPrototype/src/com/ibm/pprds/epimshw/HWPIMSNotFoundInMastException.java
@@ -4,22 +4,22 @@ import org.apache.log4j.Logger;
 
 import com.ibm.pprds.epimshw.util.LogManager;
 
-public class HWPIMSExistMastNotDefinedStkoException extends Exception {
+public class HWPIMSNotFoundInMastException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = LogManager.getLogManager().getPromoteLogger();
 
-	public HWPIMSExistMastNotDefinedStkoException() {
+	public HWPIMSNotFoundInMastException() {
 		super();
 		logException();
 	}
 
-	public HWPIMSExistMastNotDefinedStkoException(String s) {
+	public HWPIMSNotFoundInMastException(String s) {
 		super(s);
 		logException();
 	}
 
-	public HWPIMSExistMastNotDefinedStkoException(String s, Throwable cause) {
+	public HWPIMSNotFoundInMastException(String s, Throwable cause) {
 		super(s, cause);
 		logException();
 	}
