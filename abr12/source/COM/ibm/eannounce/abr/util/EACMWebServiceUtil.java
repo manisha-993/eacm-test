6f9ddaa41eb10f93ceed80b9bc63290762d01ed7
JianBoXu <xujianbo@cn.ibm.com>
2021-10-19 20:40:01 +0800

    EACM-5785-Create SSL connection to RFC sercer

---
+++ abr12/source/COM/ibm/eannounce/abr/util/EACMWebServiceUtil.java
@@ -0,0 +1,138 @@
+/* Copyright IBM Corp. 2016 */
+package COM.ibm.eannounce.abr.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSocketFactory;
+
+import COM.ibm.opicmpdh.middleware.D;
+
+
+
+/**
+ * A Utility class to invoke RDH RFC RESTful Web Service
+ * 
+ */
+public class EACMWebServiceUtil
+{
+    
+    // private static Logger log = Logger.getInstance(RdhWebServiceUtil.class);
+    private static SSLSocketFactory sslSocketFactory = null;
+
+    /***
+     * Invoke RDH RESTful Web Service
+     * 
+     * @param input Json format data
+     * @param serviceName RFC Web Service name
+     * @return result string with Json format
+     * @throws IOException
+     * @throws Exception
+     */
+    public static String callService(String input, String serviceName,StringBuffer debugSb) throws Exception
+    {
+        HttpURLConnection connection = null;
+        OutputStream os = null;
+        StringBuffer output = new StringBuffer();
+
+        String uri = RfcConfigProperties.getServiceURI();
+        String trustStore = RfcConfigProperties.getServiceTruststore();
+        String trustStorepw = RfcConfigProperties.getServiceTruststorePassword();
+        String keyStore = RfcConfigProperties.getServiceKeystore();
+        String keyStorepw = RfcConfigProperties.getServiceKeystorePassword();
+
+        String keyStoreType = RfcConfigProperties.getServiceKeystoreType();
+        String algorithm = RfcConfigProperties.getSSLAlgorithm();
+        D.ebug("service.uri = " + uri);
+       // lo(debugSb, "service.uri = " + uri);
+        D.ebug( "service.truststore = " + trustStore);
+        D.ebug("service.keystore = " + keyStore);
+        
+
+        if (sslSocketFactory == null)
+        {
+            EACMKeyManager keyManager = null;
+            EACMTrustManager trustManager = null;
+            EACMSSLContext context = null;
+            try
+            {
+                keyManager = new EACMKeyManager(keyStoreType.toUpperCase(), keyStore, keyStorepw);
+                trustManager = new EACMTrustManager(keyStoreType.toUpperCase(), trustStore, trustStorepw);
+                context = new EACMSSLContext(algorithm.toUpperCase(), keyManager, trustManager);
+                sslSocketFactory = context.getSSLContext().getSocketFactory();
+            } catch (Exception e)
+            {
+            	  
+              //  log.error("Failed to load certification information: ", e);
+                throw e;
+            }
+        }
+
+        long start = System.currentTimeMillis();
+        try
+        {
+           // log.debug("WebService = " + uri + serviceName);
+
+            URL url = new URL(uri + serviceName);
+            connection = (HttpURLConnection) url.openConnection();
+
+            if (connection instanceof HttpsURLConnection)
+            {
+                ((HttpsURLConnection) connection).setSSLSocketFactory(sslSocketFactory);
+            }
+
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            os = connection.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK)
+            {
+            	D.ebug(D.EBUG_ERR,"Invoke WebService " + serviceName + " Failed: HTTP error code = "
+                        + connection.getResponseCode() + " Http response message = "
+                        + connection.getResponseMessage());
+                throw new RuntimeException("Invoke WebService " + serviceName + " Failed: HTTP error code = "
+                        + connection.getResponseCode() + " Http response message = "
+                        + connection.getResponseMessage());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+
+            String data;
+            while ((data = br.readLine()) != null)
+            {
+                output.append(data);
+            }
+
+           D.ebug("WebService: " + serviceName + " Input data: " + input + " Output data: "
+                    + output.toString());
+        } catch (IllegalArgumentException e)
+        {
+        	D.ebug(D.EBUG_ERR,"Failed to load certification information: "+e.getStackTrace());
+            throw e;
+        } finally
+        {
+            if (os != null)
+            {
+                os.close();
+            }
+            if (connection != null)
+            {
+                connection.disconnect();
+            }
+        }
+        D.ebug(serviceName + " return : " + output.toString());
+        
+        D.ebug(serviceName + " take time: " + (System.currentTimeMillis() - start));
+
+        return output.toString();
+    }
+}
---
+++ abr12/source/COM/ibm/eannounce/abr/util/RfcConfigProperties.java
@@ -0,0 +1,145 @@
+
+
+
+package COM.ibm.eannounce.abr.util;
+
+ 
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import COM.ibm.opicmpdh.middleware.taskmaster.Log;
+
+/**
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ *
+ */
+
+public final class RfcConfigProperties extends Properties {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// Class constants
+	private static final String PROPERTIES_FILENAME = "rfc.properties";
+	// Class variables
+	private static Properties c_prop = null;
+
+	
+	/**
+	 * Some class level initialization
+	 */
+	static {
+		// Load the properties from file so they are available for each accessor method
+		reloadProperties();
+	}
+
+	/**
+	 * Don't let anyone instantiate this class.
+	 */
+	private RfcConfigProperties() {
+	}
+	/**
+	 * Load the TaskMaster properties from the taskmaster.properties file
+	 */
+	private static final void loadProperties() {
+
+		FileInputStream inProperties = null;
+		try {
+			if (c_prop == null) {
+				c_prop = new Properties();
+
+				inProperties =
+					new FileInputStream(PROPERTIES_FILENAME);
+
+				c_prop.load(inProperties);
+			}
+		} catch (Exception x) {
+			
+			Log.out(
+				"Unable to loadProperties for "
+					+ PROPERTIES_FILENAME
+					+ " "
+					+ x);
+		} finally {
+			if (inProperties != null) {
+				try {
+					inProperties.close();
+				} catch (java.io.IOException ex){
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Reload the properties from the middleware.server.properties file
+	 */
+	public static final void reloadProperties() {
+		loadProperties();
+	}
+	
+	 /* String uri = ConfigUtils.getProperty("rdh.service.uri");
+      String trustStore = ConfigUtils.getProperty("rdh.service.truststore");
+      String trustStorepw = ConfigUtils.getProperty("rdh.service.truststore.password");
+      String keyStore = ConfigUtils.getProperty("rdh.service.keystore");
+      String keyStorepw = ConfigUtils.getProperty("rdh.service.keystore.password");
+
+      String keyStoreType = ConfigUtils.getProperty("rdh.keystore.type");
+      String algorithm = ConfigUtils.getProperty("rdh.ssl.algorithm");*/
+	/**
+	 * Return the DB gate delay
+	 *
+	 * @return long
+	 */
+	public static final String getServiceURI() {
+		reloadProperties();
+
+		return 
+			c_prop.getProperty("rdh.service.uri", "");
+	}
+	/**
+	 * Return
+	 *
+	 * @return int
+	 */
+	public static final String getServiceTruststore() {
+		reloadProperties();
+		return 
+			c_prop.getProperty("rdh.service.truststore", "");
+	}
+	public static final String getServiceTruststorePassword() {
+		reloadProperties();
+		return 
+			c_prop.getProperty("rdh.service.truststore.password", "");
+	}
+	
+	public static final String getServiceKeystore() {
+		reloadProperties();
+		return 
+			c_prop.getProperty("rdh.service.keystore", "");
+	}
+	public static final String getServiceKeystorePassword() {
+		reloadProperties();
+		return 
+			c_prop.getProperty("rdh.service.keystore.password", "");
+	}
+	public static final String getServiceKeystoreType() {
+		reloadProperties();
+		return 
+			c_prop.getProperty("rdh.keystore.type", "");
+	}
+	public static final String getSSLAlgorithm() {
+		reloadProperties();
+		return 
+			c_prop.getProperty("rdh.ssl.algorithm", "");
+	}
+	/**
+	 * Return the date/time this class was generated
+	 * @return the date/time this class was generated
+	 */
+	public static final String getVersion() {
+		return "$Id: TaskMasterProperties.java,v 1.29 2013/09/11 18:40:37 wendy Exp $";
+	}
+}
