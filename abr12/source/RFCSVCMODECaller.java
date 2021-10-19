eaf9b28ba13d6de594df2d5b1ef11ab49787b973
JianBoXu <xujianbo@cn.ibm.com>
2021-10-19 20:41:05 +0800

    EACM-5785 add caller code

--- abr12/source/RFCSVCMODECaller.java
+++ abr12/source/RFCSVCMODECaller.java
@@ -1,4 +1,13 @@
 
 public class RFCSVCMODECaller {
 
+	/**
+	 * 1.	Call TssMatmCreate to create the material master f	or the product object.
+	 */
+	public void TssMatmCreate (){
+		
+	}
+	public void TssClassificationMaint (){
+		
+	}
 }
