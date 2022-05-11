79d418892c87bf044fa1bc19c78adbf0d80edd5c
xujianbo <xujianbo@cn.ibm.com>
2022-05-11 22:32:35 +0800

    fixed MODELCONVERt issue

--- abr12/source/COM/ibm/eannounce/abr/sg/adsxmlbh1/MODELCONVERTIERPABRSTATUS.java
+++ abr12/source/COM/ibm/eannounce/abr/sg/adsxmlbh1/MODELCONVERTIERPABRSTATUS.java
@@ -121,7 +121,6 @@ public class MODELCONVERTIERPABRSTATUS extends PokBaseABR {
 			// build the text file
 
 			Connection connection = m_db.getODSConnection();
-			Connection odsConnection = m_db.getODSConnection();
 			PreparedStatement statement = connection.prepareStatement(CACEHSQL);
 			statement.setInt(1, rootEntity.getEntityID());
 			ResultSet resultSet = statement.executeQuery();
@@ -132,10 +131,14 @@ public class MODELCONVERTIERPABRSTATUS extends PokBaseABR {
 			if (xml != null) {
 				
 				 MODELCONVERT modelconvert = XMLParse.getObjectFromXml(xml, MODELCONVERT.class); 
-				String modelXML = getModelFromXML(abrversion, rootDesc, odsConnection);
-				 	MODEL model = XMLParse.getObjectFromXml(modelXML,MODEL.class );
+				String modelXML = getModelFromXML(modelconvert.getTOMACHTYPE(), modelconvert.getTOMODEL(), connection);
+						addDebug("MODEL xml:"+convertToHTML(modelXML));
+				if(modelXML==null) {
+					throw new RuntimeException("MODEL xml not found in cache fro MODEL:"+modelconvert.getTOMODEL()+" MACHTYPE:"+modelconvert.getTOMACHTYPE());
+				}
+				MODEL model = XMLParse.getObjectFromXml(modelXML,MODEL.class );
 				 if (modelconvert.getFROMMACHTYPE().equals(modelconvert.getTOMACHTYPE())) {
-					 ChwModelConvertMtc mtc = new ChwModelConvertMtc(model,modelconvert,connection,odsConnection);
+					 ChwModelConvertMtc mtc = new ChwModelConvertMtc(model,modelconvert,m_db.getPDHConnection(),connection);
 					try {
 						mtc.execute();	
 						addOutput(mtc.getRptSb().toString());
@@ -145,7 +148,7 @@ public class MODELCONVERTIERPABRSTATUS extends PokBaseABR {
 						throw e;
 					} 
 				}else {
-					 ChwModelConvertUpg mUpg = new ChwModelConvertUpg(model,modelconvert,connection,odsConnection);
+					 ChwModelConvertUpg mUpg = new ChwModelConvertUpg(model,modelconvert,m_db.getPDHConnection(),connection);
 					 try{
 						 mUpg.execute();
 					 }catch (Exception e) {
