ccb399365ce233db66f691fa22e59aefd0d21e37
xujianbo <xujianbo@cn.ibm.com>
2022-08-22 16:24:01 +0800

    remove Bulk caller

--- abr12/source/COM/ibm/eannounce/abr/sg/adsxmlbh1/ADSABRSTATUS.java
+++ abr12/source/COM/ibm/eannounce/abr/sg/adsxmlbh1/ADSABRSTATUS.java
@@ -4222,6 +4222,8 @@ ADSATTRIBUTE    40  WARRTYPE
         		addDebug("Skip trigger IERP caller for MODEL :"+"subCat:"+subCat+" oldindc:"+oldindc);
         	}
         	else{setFlagValue("MODELIERPABRSTATUS", "0020");
+        	
+        	}
 
         	//WARRSVCCOVR ！= "WSVC02"
         	String flagString = PokUtils.getAttributeFlagValue(rootEntity, "WARRSVCCOVR");
@@ -4231,7 +4233,7 @@ ADSATTRIBUTE    40  WARRTYPE
         		setFlagValue("MODELWARRABRSTATUS", "0020");
         	}
 
-        	String tmfSQL = "select distinct f.attributevalue as BULKMESINDC "
+        	/*String tmfSQL = "select distinct f.attributevalue as BULKMESINDC "
         			+ "from opicm.relator r "
         			+ "join opicm.flag f on f.entitytype=r.entitytype and f.ENTITYID=r.entityid and f.attributecode='BULKMESINDC' and f.VALTO > current timestamp and f.EFFTO > current timestamp "
         			+ "where r.ENTITYTYPE = 'PRODSTRUCT' and r.ENTITY2ID = ? and r.VALTO > current timestamp and r.EFFTO > current timestamp with ur";
@@ -4247,8 +4249,8 @@ ADSATTRIBUTE    40  WARRTYPE
                     break;
             	}
             }
-
-        	}
+*/
+        	
 
         }else if("PRODSTRUCT".equals(getEntityType())){
         	setFlagValue("TMFIERPABRSTATUS", "0020");
@@ -4260,10 +4262,10 @@ ADSATTRIBUTE    40  WARRTYPE
         	}
         	
             String BULKMESINDC  = PokUtils.getAttributeFlagValue(rootEntity, "BULKMESINDC");
-            if("MES0001".equals(BULKMESINDC)){
+           /* if("MES0001".equals(BULKMESINDC)){
                 //PRODSTRUCT.BULKMESINDC = "MES0001" (Yes)
                 setFlagValue("TMFBULKABRSTATUS", "0020");
-            }
+            }*/
         }else if ("FEATURE".equals(getEntityType())) {
         	setFlagValue("FEATUREIERPABRSTATUS", "0020");
 		}
