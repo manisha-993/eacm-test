9b6948b8be761bb5e1c67e07f6e98663a86b6a6e
xujianbo <xujianbo@cn.ibm.com>
2023-03-09 16:31:00 +0800

    EACM-7615 Add SUO support

--- abr12/source/COM/ibm/eannounce/abr/sg/rfc/ChwMatmCreate.java
+++ abr12/source/COM/ibm/eannounce/abr/sg/rfc/ChwMatmCreate.java
@@ -237,6 +237,8 @@ public class ChwMatmCreate extends RdhBase {
 					bmmh5.get(0).setMaktx("STaaS" + " " + model.getMACHTYPE());
 				}else if("PWaaS".equals(model.getSUBGROUP())){
 					bmmh5.get(0).setMaktx("PWaaS" + " " + model.getMACHTYPE());
+				}else if("SUO".equals(model.getSUBGROUP())){
+					bmmh5.get(0).setMaktx("\"SUO\"" + " " + model.getMACHTYPE()+" (for Subgroup SUO)");
 				}else {
 					bmmh5.get(0).setMaktx("MACHINE TYPE " + model.getMACHTYPE() + " - Model NEW");
 				}
