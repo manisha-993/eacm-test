5690806fdf66e70da973d3dfb487651c93d4b583
yuzgyg <yuzgyg@cn.ibm.com>
2023-03-17 15:01:25 +0800

    EACM-7582 Secret Detect

--- abr12/source/COM/ibm/eannounce/abr/sg/adsxmlbh1/MODELGARSABRSTATUS.java
+++ abr12/source/COM/ibm/eannounce/abr/sg/adsxmlbh1/MODELGARSABRSTATUS.java
@@ -112,7 +112,7 @@ public class MODELGARSABRSTATUS extends PokBaseABR {
                         this.addOutput(bom.getRFCName() + " called successfully!");
                     } else {
                         this.addOutput(bom.getRFCName() + " called  faild!");
-                        this.addDebug(bom.getRFCName() + " webservice return code:" + bom.getRfcrc());
+                        this.addDebug(bom.getRFCName() + " webservice return code: " + bom.getRfcrc());
                         this.addOutput(bom.getError_text());
                     }
                     updateParkStatus = new UpdateParkStatus("MD_CHW_IERP", bom.getRFCNum());
