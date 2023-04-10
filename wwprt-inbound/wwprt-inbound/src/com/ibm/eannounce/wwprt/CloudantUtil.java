0e2b475a0501e725dc2471cefe0bdadee55203f9
xujianbo <xujianbo@cn.ibm.com>
2023-04-10 11:42:54 +0800

    EACM-7570 Add new catcher, pull data from Cloudant DB

---
+++ wwprt-inbound/wwprt-inbound/src/com/ibm/eannounce/wwprt/CloudantUtil.java
@@ -0,0 +1,145 @@
+package com.ibm.eannounce.wwprt;
+
+import com.ibm.cloud.cloudant.v1.Cloudant;
+import com.ibm.cloud.cloudant.v1.model.PostFindOptions;
+import com.ibm.cloud.sdk.core.security.BasicAuthenticator;
+import com.ibm.mq.*;
+
+import java.io.FileInputStream;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Properties;
+
+public class CloudantUtil {
+
+	private static String apiKey;
+	private static String cloudant_host;
+	private static String cloudant_password;
+	private static String cloudant_dbname;
+	private static int span;
+	private static int size;
+	private static String propertiesFile="test.properties";
+	private static boolean environmentSet;
+	private static Cloudant client;
+	private static DateTimeFormatter cloudant_time_formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.nnnnnnnnnnnnXXX");
+
+	private static int delay = 0;
+
+
+	public CloudantUtil() {
+		setup();
+	}
+	public CloudantUtil(String properties) {
+		propertiesFile = properties;
+		setup();
+	}
+
+	public static void setup(String properties) {
+		propertiesFile = properties;
+		setup();
+	}
+	@SuppressWarnings("unchecked")
+	private static void setup() {
+		try {
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(propertiesFile));
+			apiKey = properties.getProperty("cloudant.apikey");
+			cloudant_host = properties.getProperty("cloudant.host");
+			cloudant_password= properties.getProperty("cloudant.password");
+			cloudant_dbname = properties.getProperty("cloudant.dbname");
+			span = Integer.parseInt(properties.getProperty("cloudant.span","4"));
+			size = Integer.parseInt(properties.getProperty("cloudant.size","200"));
+			delay = Integer.parseInt(properties.getProperty("delay", "0"));
+
+			BasicAuthenticator authenticator = new BasicAuthenticator.Builder()
+					.username(apiKey)
+					.password(cloudant_password)
+					.build();
+			client = new Cloudant(Cloudant.DEFAULT_SERVICE_NAME,authenticator);
+			client.setServiceUrl(cloudant_host);
+
+			environmentSet = true;
+		} catch (Throwable e) {
+			e.printStackTrace();
+			Log.e( "Unable to setup MQ Environment properties", e);
+			environmentSet = false;
+			throw new IllegalStateException("Unable to setup MQ Environment properties.", e);
+		}
+	}
+
+	public static Cloudant getClient(){
+		if(client==null){
+			setup();
+		}
+		return client;
+	}
+
+	public static int getSpan() {
+		return span;
+	}
+
+	public static void setSpan(int span) {
+		CloudantUtil.span = span;
+	}
+
+	public static int getSize() {
+		return size;
+	}
+
+	public static void setSize(int size) {
+		CloudantUtil.size = size;
+	}
+
+	public static PostFindOptions getFindOptions(Map<String, Object> selector , long skip, int limit){
+		PostFindOptions findOptions = new PostFindOptions.Builder()
+				.db(cloudant_dbname).selector(selector).
+				limit(limit).
+				skip(skip).
+				build();
+		return findOptions;
+	}
+
+	public void close() {
+		if (!environmentSet) {
+			throw new IllegalStateException("MQ Environment not set. Call MQ.setup() before.");
+		}
+
+		try {
+
+		} catch (Exception e) {
+			Log.e( "Unable to close MQ Environment", e);
+		}
+	}
+	public static String getNow(){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(("yyyy-MM-dd'T'HH:mm:ss.nnnnnnnnnXXX"));
+		// OffsetDateTime  zonedDateTime = OffsetDateTime.parse("2023-02-03T21:03:54.465000000000+00:00", formatter);
+		String dateTimeString = "2023-02-03T21:03:54.465000000000+00:00";
+		System.out.println(dateTimeString.length());
+		OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateTimeString.substring(0, 29) + dateTimeString.substring(32), formatter);
+		System.out.println(offsetDateTime);
+		return null;
+	}
+	public static String[] getPreviousTimePeriod(int span) {
+		String timePeriod[] = new String[2];
+		OffsetDateTime now = OffsetDateTime.now(); // 获取当前时间
+		int hour = now.getHour(); // 获取当前小时数
+		if (hour < span) { // 如果当前时间小于4小时
+			now = now.minusHours(span); // 将当前时间减去1天
+		}
+		int roundedHour = hour - (hour % span); // 计算往前最近的4的倍数小时数
+		OffsetDateTime t1 = now.withHour(roundedHour).withMinute(0).withSecond(0).withNano(0); // 设置整点时间
+		OffsetDateTime t2 = t1.plusHours(span);
+		timePeriod[0]=t1.format(cloudant_time_formatter);;
+		timePeriod[1]=t2.format(cloudant_time_formatter);
+		return timePeriod;
+	}
+
+	public static void main(String[] args) {
+		getNow();
+	}
+	public interface MessageListener {
+		public void readMessage(MQMessage msg) throws Exception;
+		public void noMessagesToRead() throws Exception;
+	}
+}
---
+++ wwprt-inbound/wwprt-inbound/src/com/ibm/eannounce/wwprt/catcher/CloudantListener.java
@@ -0,0 +1,107 @@
+package com.ibm.eannounce.wwprt.catcher;
+
+import com.ibm.eannounce.wwprt.*;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CloudantListener implements Runnable {
+
+	private boolean running;
+	private String t1=null;
+	private String t2=null;
+
+	private long interval = 10000;
+
+	private CloudantCatcher catcher;
+
+	public String getT1() {
+		return t1;
+	}
+
+	public void setT1(String t1) {
+		this.t1 = t1;
+	}
+
+	public String getT2() {
+		return t2;
+	}
+
+	public void setT2(String t2) {
+		this.t2 = t2;
+	}
+
+	public CloudantListener() {
+		catcher = new CloudantCatcher();
+		catcher.initConnection();
+	}
+	
+	public void setInterval(long interval) {
+		this.interval = interval;
+	}
+
+
+	public void start() {
+		if (running) {
+			Log.i("Catcher is already running");
+		} else {
+			Thread thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	public void stop() {
+		running = false;
+	}
+
+	public void run() {
+		running = true;
+		Log.i("Catcher is now running...");
+		while (running) {
+			try {
+				int span = CloudantUtil.getSpan();
+				if(t1==null||t2==null){
+				 String[] times=getPreviousTimePeriod(span);
+				 t1 = times[0];
+				 t2=times[1];
+				}
+				Log.i("Pulling data t1="+t1+" t2="+t2);
+				pullPrice(t1,t2);
+				onCheck();
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {
+			} catch (Throwable e) {
+				Log.e( "Error on catcher loop", e);
+				running = false;
+			}
+			if (!running)
+				break;
+		}
+		Log.i("Catcher has stopped.");
+		onStop();
+	}
+	
+	public void onCheck() {}
+	
+	public void onStop() {}
+
+	public void pullPrice(String t1,String t2){
+		catcher.pullPricetoXML(t1,t2);
+	}
+	public static String[] getPreviousTimePeriod(int span) {
+		String timePeriod[] = new String[2];
+		OffsetDateTime now = OffsetDateTime.now();
+		int hour = now.getHour();
+		if (hour < span) {
+			now = now.minusHours(span);
+		}
+		int roundedHour = hour - (hour % span);
+		OffsetDateTime t1 = now.withHour(roundedHour).withMinute(0).withSecond(0).withNano(0); // 设置整点时间
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.nnnnnnnnnnnnXXX");
+		OffsetDateTime t2 = t1.plusHours(span);
+		timePeriod[0]=t1.format(formatter);;
+		timePeriod[1]=t2.format(formatter);
+		return timePeriod;
+	}
+	
+}
